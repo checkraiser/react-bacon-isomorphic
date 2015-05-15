1dcbc77aa22ba8840e895ec80fdce5708f29a90a
Matti Lankinen <m.lankinen@iki.fi>
2015-05-16 00:37:42 +0300

    Use server to store items

--- app/site/app.js
+++ app/site/app.js
@@ -3,15 +3,18 @@ const React   = require('react'),
       Bacon   = require('baconjs'),
       TodoApp = require('./todoApp'),
       todos   = require('./todos'),
-      filter  = require('./filter')
+      filter  = require('./filter'),
+      loader  = require('./loader')
 
 
-const filterP = filter.toProperty(window.location.hash.substring(1) || 'all'),
-      itemsP  = todos.toItemsProperty(window.INITIAL_MODEL.items, filterP)
+const filterP  = filter.toProperty(window.location.hash.substring(1) || 'all'),
+      itemsP   = todos.toItemsProperty(window.INITIAL_MODEL.items, filterP),
+      loadingP = loader.toProperty()
 
 const appState = Bacon.combineTemplate({
   items: itemsP,
-  filter: filterP
+  filter: filterP,
+  loading: loadingP
 })
 
 appState.onValue((state) => {
