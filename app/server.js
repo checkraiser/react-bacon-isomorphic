03e5cbbe8d36390a9839ac00e4b9b5989d2389ca
Matti Lankinen <m.lankinen@iki.fi>
2015-05-15 23:47:55 +0300

    Use serve-static

--- app/server.js
+++ app/server.js
@@ -1,13 +1,15 @@
-const express = require('express'),
-      fs      = require('fs'),
-      path    = require('path'),
-      React   = require('react'),
-      TodoApp = require('./site/todoApp'),
-      app     = express()
+const express     = require('express'),
+      fs          = require('fs'),
+      {resolve}   = require('path'),
+      serveStatic = require('serve-static'),
+      React       = require('react'),
+      TodoApp     = require('./site/todoApp'),
+      app         = express()
 
 
-const _index = fs.readFileSync(path.resolve(__dirname, '../index.html')).toString()
+const _index = fs.readFileSync(resolve(__dirname, '../index.html')).toString()
 
+app.use('/public', serveStatic(resolve(__dirname, '../public')))
 app.get('/', (req, res) => {
   const model = {
     items: [
@@ -19,21 +21,6 @@ app.get('/', (req, res) => {
   res.send(_index
     .replace('{{APP}}', React.renderToString(<TodoApp {...model} />))
     .replace('{{INITIAL_MODEL}}', JSON.stringify(model)))
-})
-
-app.get('/styles/base.css', (req, res) => {
-  res.set('Content-Type', 'text/css')
-  res.sendFile(path.resolve(__dirname, '../node_modules/todomvc-common/base.css'))
-})
-
-app.get('/styles/index.css', (req, res) => {
-  res.set('Content-Type', 'text/css')
-  res.sendFile(path.resolve(__dirname, '../node_modules/todomvc-app-css/index.css'))
-})
-
-app.get('/js/app.js', (req, res) => {
-  res.set('Content-Type', 'text/javascript')
-  res.sendFile(path.resolve(__dirname, '../build/bundle.js'))
 })
 
 app.listen(3000, () => console.log('Server listening on port 3000'))
