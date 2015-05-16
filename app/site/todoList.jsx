7aca6b3f4324178d1234ea4f35b142075c40d0c3
Matti Lankinen <m.lankinen@iki.fi>
2015-05-16 13:58:41 +0300

    Disable app functions while saving data to the server

--- app/site/todoList.jsx
+++ app/site/todoList.jsx
@@ -16,9 +16,10 @@ module.exports = React.createClass({
           type="checkbox"
           checked={allCompleted}
           onChange={e => todos.setAllCompleted(e.target.checked)}
+          disabled={this.props.loading}
           />
         <ul id="todo-list">
-          {R.map(it => it.display ? <TodoItem key={it.id} item={it} /> : '', this.props.items)}
+          {R.map(it => it.display ? <TodoItem key={it.id} item={it} loading={this.props.loading} /> : '', this.props.items)}
         </ul>
       </section>
     )
